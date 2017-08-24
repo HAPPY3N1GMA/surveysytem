ec1513b0fcced4fefcddbfc204fee1d40b31a267
Keiran Sampson <keiran.sampson@gmail.com>
2017-08-25 00:28:21 +1000

    templates & login page v1

---
+++ source/routes.py
@@ -0,0 +1,40 @@
+import csv
+from flask import Flask, redirect, render_template, request, url_for
+from server import app, users
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+	if request.method == "POST":
+
+		user = request.form["username"]
+		pwd = request.form["password"]
+
+		# Check Password
+
+		if check_password(user,pwd):
+			print("Valid password")
+
+			#render to main page
+
+		else:
+
+			print("Incorrect Password")
+			#make it render saying invalid username/password
+			return render_template("index.html", invalid=True)
+
+	return render_template("index.html", invalid=False)
+
+
+def check_password(user, pwd):
+
+	#if you dont use .get to look inside the dictionary
+	#you will get errors if the username is not there
+	#.get will return none which is seen as false by the statement
+
+	if pwd == users.get(user):
+		return True
+	else:
+		return False
+
+
+
