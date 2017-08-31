2fb065dbd82c7d6cfe18f4888aa155bf24c53a52
Keiran Sampson <keiran.sampson@gmail.com>
2017-08-31 15:33:55 +1000

    functions folder

--- source/routes.py
+++ source/routes.py
@@ -1,7 +1,7 @@
 import csv, ast, os, time
 from flask import Flask, redirect, render_template, request, url_for
 from server import app, users
-
+from functions import append
 
 authenticated = 0
 
@@ -102,36 +102,3 @@ class csvfile(IDfile):
 			for row in reader:
 				namelist.append(row)
 			return namelist
-
-
-
-# 	append_answer Function temp readme
-#	
-#	appends a new answer result to a question for a specified survey
-# 	
-#	Note that this expects a survey file and will error if missing
-#
-# 	Example CSV File Format:
-#
-#	question_id,answers
-#	1,"['a', 'a', 'b', 'b', 'c', 'a', 'd', 'j']"
-#
-# 	append("test_answers",1,"j") -- will open file test_answers.csv, 
-# 	and append the answer j to question 1
-
-def append_answer(s_id, q_id, answer):
-	file = str(s_id)+".csv"
-	tmp = str(s_id)+"_tmp.csv"	
-	with open(file, 'r+') as csvReadFile:
-		fieldnames = ['question_id', 'answers']
-		reader = csv.DictReader(csvReadFile, fieldnames=fieldnames)
-		with open (tmp, 'w') as write_row:
-			writer=csv.DictWriter(write_row, fieldnames=fieldnames)
-			for row in reader:
-				if(row['question_id']==q_id):
-					tmp_list=ast.literal_eval(row['answers'])
-					tmp_list.append(answer)
-					row['answers'] = tmp_list
-				writer.writerow(row)				
-	os.remove(file)
-	os.rename(tmp, file)
