2a420f38d47a00793bff88f5ab65b64567270649
mkadeline <z5157128@ad.unsw.edu.au>
2017-10-04 10:09:09 +1100

    updated qs

--- source/defines.py
+++ source/defines.py
@@ -1,5 +1,5 @@
-#Global object class lists
-debug = True #set to true to reset db on restarts
+# Global object class lists
+debug = False  # set to true to reset db on restarts
 masterSurveys = []
 masterQuestions = []
 masterCourses = []
