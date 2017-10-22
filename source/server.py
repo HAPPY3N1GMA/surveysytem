cf6ae3ba25a2638ed4f1bffb31ee59b13fee08b4
Keiran Sampson <keiran.sampson@gmail.com>
2017-10-22 23:06:18 +1100

    removed dependancy

--- source/server.py
+++ source/server.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
 from models import users_model, surveys_model
-import schedule
-import time
-import _thread
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
